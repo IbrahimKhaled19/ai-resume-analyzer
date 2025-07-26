0cc491968e9777b9a339be1c0dae60170c376404
Ibrahim <ibrahimaboaly@gmail.com>
2025-07-26 03:05:33 +0300

    Implement Wipe Function

--- app/routes.ts
+++ app/routes.ts
@@ -4,5 +4,6 @@ import { title } from "process";
 export default [index("routes/home.tsx"),
     route('/auth',"routes/auth.tsx"),
     route('/upload' , "routes/upload.tsx"),
-    route('/resume/:id' , "routes/resume.tsx")
+    route('/resume/:id' , "routes/resume.tsx"),
+    route('/wipe' , "routes/wipe.tsx")
 ] satisfies RouteConfig;
